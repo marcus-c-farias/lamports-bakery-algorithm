43d88d48545c0b05107dbc15de7bc2938e7b272a
marcus-c-farias <marcusczupriniakifarias@gmail.com>
2025-04-20 19:04:29 -0300

    add lamport lib

--- solution/lamport_mutex.c
+++ solution/lamport_mutex.c
@@ -1,16 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lamport_mutex.h"
 
-void lamport_mutex_lock(int thread_id){
+static int *choosing;
+static int *ticket;
+static int num_threads = 0;
 
+void lamport_mutex_init(int n_threads) {
 
+  int i;
+  num_threads = n_threads;
+  
+  choosing = malloc(n_threads * sizeof(int));
+  ticket = malloc(n_threads * sizeof(int));
+
+  if (!choosing || !ticket) {
+    fprintf(stderr, "Erro ao alocar memória para o mutex\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for(i=0; i<num_threads; i++) {
+    choosing[i] = 0;
+    ticket[i] = 0;
+  }
 }
-void lamport_mutex_unlock(int thread_id){
 
-
-
+int max_ticket() {
+  int i, max = ticket[0];
+  for (i = 1; i < num_threads; i++) {
+    max = ticket[i] > max ? ticket[i] : max;
+  }
+  
+  return max;
 }
-void lamport_mutex_init(){
 
+void lamport_mutex_lock(int thread_id) {
+    
+    int j, i = thread_id;
 
+    choosing[i] = 1;
+    ticket[i] = max_ticket() + 1;
+    choosing[i] = 0;
+    for (j = 0; j < num_threads; j++) {
+        while (choosing[j]) 
+        while (ticket[j] != 0 && (
+              (ticket[j] < ticket[i]) || (ticket[j] == ticket[i] && j < i))
+        );
+    }
+}
 
+void lamport_mutex_unlock(int thread_id) {
+  ticket[thread_id] = 0;
+}
+
+void lamport_mutex_destroy() {
+  free(choosing);
+  free(ticket);
 }
--- solution/lamport_mutex.h
+++ solution/lamport_mutex.h
@@ -1,8 +1,11 @@
 #ifndef lamport_mutex  
 #define lamport_mutex
 
+//#define MAX_THREADS 1000
+
 void lamport_mutex_init();
 void lamport_mutex_lock(int thread_id);
 void lamport_mutex_unlock(int thread_id);
+void lamport_mutex_destroy();
 
 #endif
